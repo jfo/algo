f656ca066ac044df3f906af6b05c751907e05ff6
Jeff Fowler <jeffowler@gmail.com>
2018-02-25 20:23:54 +0100

    random u64s

--- init.zig
+++ init.zig
@@ -2,14 +2,19 @@ const std = @import("std");
 const os = std.os;
 const warn = std.debug.warn;
 
-pub fn main() void {
-    var bytes: [20]u8 = undefined;
+fn randomu64() u64 {
+    var bytes: [4]u8 = undefined;
     os.getRandomBytes(bytes[0..]) catch |err| { };
+    return  u64(bytes[0]) << 24 | u64(bytes[1]) << 16 | u64(bytes[2]) << 8 | u64(bytes[3]);
+}
 
-    var i:u8 = 0;
-    while (i < bytes.len) : (i += 4) {
-        warn("{}\n",
-            u64(bytes[i]) << 24 | u64(bytes[i + 1]) << 16 | u64(bytes[i + 2]) << 8 | u64(bytes[i + 3])
-        );
+pub fn main() void {
+    const numOfRandomNumbers = 100;
+    var randomu64s: [numOfRandomNumbers]u64 = undefined;
+
+    var i:u16 = 0;
+    while (i < randomu64s.len) : (i += 1) {
+        randomu64s[i] = randomu64();
+        warn("{}\n", randomu64s[i]);
     }
 }
