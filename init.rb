c4063b8fc8abccfa01147d888055ad85799bda4f
Jeff Fowler <jeffowler@gmail.com>
2018-03-18 13:38:43 +0100

    start mergeing

--- init.rb
+++ init.rb
@@ -1,2 +1,17 @@
-data = File.open("./data/randomstuff", "r").read;
-p data.unpack("Q*").length;
+data = File.open("./data/randomstuff", "r").read.unpack("Q*")
+p data
+
+def merge(arr1, arr2)
+  arr1 | arr2
+end
+
+def merge_sort(arr)
+  if arr.count < 2
+    p arr
+    return arr
+  else
+    return merge(merge_sort(arr[0..(arr.count/2 - 1)]), merge_sort(arr[(arr.count/2)..-1]))
+  end
+end
+
+p merge_sort(data) == data
